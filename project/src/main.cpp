640962059047dcf65e073e8b4a46a85d9f7b2716
Bacal Mesfin <mesfinb2@vcu.edu>
2024-03-01 17:21:14 -0500

    changed googletest dep to reference a release instead of the main branch

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -5,10 +5,6 @@ int main(int argc, char **argv){
   const foo::bar b(32);
   std::println("{}",b);
   std::println("{:#x}",b);
-
-
-  // const baz::fizz t(44);
-  // std::cout << t << '\n';
   return 0;
 }
 
